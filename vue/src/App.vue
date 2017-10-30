f85d0fbdbd8565ada01d2635adf5e9d09e82b82a
Mitch <dennison.mitch@gmail.com>
2017-10-30 16:38:27 -0400

    Initial Home Page
    First elements of Home Page added

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,21 +1,20 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
+  <v-app light>
+    <!--<v-navigation-drawer
       persistent
       :mini-variant="miniVariant"
       :clipped="clipped"
       v-model="drawer"
       enable-resize-watcher
-      app
     >
       <v-list>
         <v-list-tile
           value="true"
           v-for="(item, i) in items"
-          :key="i"
+          :key="i" :to="item.url"
         >
           <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
+            <v-icon light v-html="item.icon"></v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title v-text="item.title"></v-list-tile-title>
@@ -23,60 +22,25 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
+    <v-toolbar fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
+      <v-btn
+        icon
+        light
+        @click.stop="miniVariant = !miniVariant"
+      >
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
+    </v-toolbar>-->
     <main>
-      <v-content>
-        <v-container fluid>
-          <v-slide-y-transition mode="out-in">
-            <v-layout column align-center>
-              <img src="/static/v.png" alt="Vuetify.js" class="mb-5">
-              <blockquote>
-                &#8220;First, solve the problem. Then, write the code.&#8221;
-                <footer>
-                  <small>
-                    <em>&mdash;John Johnson</em>
-                  </small>
-                </footer>
-              </blockquote>
-            </v-layout>
-          </v-slide-y-transition>
-        </v-container>
-      </v-content>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <router-view></router-view>
+        </v-slide-y-transition>
+      </v-container>
     </main>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
   </v-app>
 </template>
 
@@ -87,16 +51,13 @@
         clipped: false,
         drawer: true,
         fixed: false,
-        items: [{
-          icon: 'bubble_chart',
-          title: 'Inspire'
-        }],
+        items: [
+          { icon: 'home', title: 'Home', url: '/' },
+        ],
         miniVariant: false,
         right: true,
         rightDrawer: false,
-        title: 'Vuetify.js'
       }
     }
   }
-
 </script>
---
+++ vue/src/components/Home.vue
@@ -0,0 +1,102 @@
+<template>
+    <main>
+          <v-flex xs12 class="my-3" >
+              <v-toolbar color="elevation-0">
+                <!--<v-toolbar-side-icon></v-toolbar-side-icon>-->
+                <v-toolbar-title>Computing in Community</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+          </v-flex>
+
+      <section>
+        <v-parallax :src="require('@/assets/Library.jpg')" height = "500">
+          <v-layout
+            column
+            align-center
+            justify-center
+            class="white--text"
+          >
+            <img :src="require('@/assets/BellTower.png')" alt="Furman" height="300">
+            <!--<h1 class="white--text mb-2 display-3">Computing in Community</h1>-->
+          </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-layout
+          column
+          wrap
+          class="my-5"
+          align-center
+        >
+          <v-flex xs12 sm8 class="my-3">
+            <div class="text-xs-center">
+              <h2 class="headline">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+                id est laborum.</h2>
+            </div>
+          </v-flex>
+          <v-flex xs12>
+            <v-container grid-list-xl>
+              <v-layout row wrap align-center>
+                <v-flex xs12 md6>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">school</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">About</div>
+                    </v-card-title>
+                    <v-card-text>
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md6>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">people</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline">Students</div>
+                    </v-card-title>
+                    <v-card-text>
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md6>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">play_arrow</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Faculty</div>
+                    </v-card-title>
+                    <v-card-text>
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md6>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">play_arrow</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Community Partners</div>
+                    </v-card-title>
+                    <v-card-text>
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-flex>
+        </v-layout>
+      </section>
+
+
+    </main>
+</template>
