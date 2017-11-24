a40e4e50e301f423ff1621c4a57cba00b944f99c
Mitch <dennison.mitch@gmail.com>
2017-11-24 14:07:20 -0500

    Example
    Commented out stuff making it not work and put an example event array in (temporary)

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -20,7 +20,7 @@
         <v-container fill-height fluid>
           <v-flex xs12 sm6 offset-sm3>
             <v-card>
-              <full-calendar :events="eventData" locale="en"></full-calendar>
+              <full-calendar :events="events" locale="en"></full-calendar>
             </v-card>
           </v-flex>
         </v-container>
@@ -32,64 +32,76 @@
 </template>
 
 <script>
-import axios from 'axios';
-export default {
+  import axios from 'axios';
+  export default {
   data () {
-  return {
-    max25chars: (v) => v.length <= 25 || 'Input too long!',
-        tmp: '',
-        search: '',
-        pagination: {},
-        eventData: [
-          { text: 'Event', value: 'name' },
-          { text: 'Host', value: 'hostName' },
-          { text: 'Description', value: 'description' },
-          { text: 'Start Date', value: eventSplit(startTime)[0]},
-          { text: 'End Date', value: eventSplit(endTime)[0]},
-          { text: 'Start Time', value: eventSplit(startTime)[1]},
-          { text: 'End Time', value: eventSplit(endTime)[1]},
-          { text: 'Tags', value: 'tags'}
-        ],
-        cincevents: [],
+    return {
+      max25chars: (v) => v.length <= 25 || 'Input too long!',
+      tmp: '',
+      search: '',
+      pagination: {},
+      /*
+      eventData: [
+        { text: 'Event', value: 'name' },
+        { text: 'Host', value: 'hostName' },
+        { text: 'Description', value: 'description' },
+        { text: 'Start Date', value: eventSplit(startTime)[0]},
+        { text: 'End Date', value: eventSplit(endTime)[0]},
+        { text: 'Start Time', value: eventSplit(startTime)[1]},
+        { text: 'End Time', value: eventSplit(endTime)[1]},
+        { text: 'Tags', value: 'tags'}
+      ], */
+      cincevents: [
+      ],
+      events: [
+          {
+            title     :  'event1',
+            start     : '2017-11-01',
+            end       : '2017-11-02',
+            cssClass  : '',
+            YOUR_DATA : {}
+          },
+      ],
     }
   },
   components : {
-  'full-calendar': require('vue-fullcalendar')  
+  'full-calendar': require('vue-fullcalendar')
   },
-        created() {
-        axios.get('http://localhost:3000/events?approved=true')
-          .then(response => {
-            var size = response.data.length;
-            var i;
-            for (i = 0; i < 4; i++){
-              if (response.data[i].tags.student === 'true'){
-                this.eventData.push({
-                  name: response.data[i].name,
-                  host: response.data[i].hostName,
-                  description: response.data[i].description,
-                  startTime: response.data[i].start,
-                  endTime: response.data[i].end,
-
-                });
-              }
-              else if (response.data[i].tags.faculty === 'true'){
-                this.cincevents.push({
-                  name: response.data[i].name,
-                  host: response.data[i].hostName,
-                  description: response.data[i].description,
-                  startTime: response.data[i].start,
-                  endTime: response.data[i].end,
-                });
-              }
-            }
-          })
-      },
-      eventSplit(dateTime){
-        var dts = dateTime.split(" ");
-        var date = dts[0];
-        var time = dts[1];
-        return (date, time);
+  /*
+  created() {
+    axios.get('http://localhost:3000/events?approved=true').then(response => {
+      var size = response.data.length;
+      var i;
+      for (i = 0; i < 4; i++){
+        if (response.data[i].tags.student === 'true'){
+          this.eventData.push({
+            name: response.data[i].name,
+            host: response.data[i].hostName,
+            description: response.data[i].description,
+            startTime: response.data[i].start,
+            endTime: response.data[i].end,
+          });
+        }
+        else if (response.data[i].tags.faculty === 'true'){
+          this.cincevents.push({
+            title: response.data[i].name,
+            host: response.data[i].hostName,
+            description: response.data[i].description,
+            startTime: response.data[i].start,
+            endTime: response.data[i].end,
+          });
+        }
       }
+    })
+  },
+  methods: {
+    eventSplit: function (dateTime) {
+      var dts = dateTime.split(" ");
+      var date = dts[0];
+      var time = dts[1];
+      return (date, time);
+    },
+  }, */
 }
 
 </script>
