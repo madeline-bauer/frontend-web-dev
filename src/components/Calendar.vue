c91851d506dc34a237b6add73aec6bc52c7556c8
Mitch <dennison.mitch@gmail.com>
2017-11-27 16:57:12 -0500

    New Calendar
    We're using vue-event-calendar yay

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,6 +1,5 @@
 <template>
   <main>
-
     <section>
       <v-parallax :src="require('@/assets/purple.png')" height = "400">
         <v-layout
@@ -18,100 +17,50 @@
     <section>
       <v-layout>
         <v-container fill-height fluid>
-          <v-flex xs12 sm6 offset-sm3>
+          <v-flex xs12 md8 offset-md2>
             <v-card>
-              <full-calendar :events="cincevents" locale="en"
-                @eventClick="eventClick"
-                @dayClick="dayClick"
-                @moreClick="moreClick">
-              </full-calendar>
+              <vue-event-calendar :events="cincEvents"></vue-event-calendar>
             </v-card>
           </v-flex>
         </v-container>
       </v-layout>
     </section>
-
   </main>
-
 </template>
 
 <script>
-  import axios from 'axios';
-  export default {
+import axios from 'axios';
+export default {
   data () {
     return {
-      max25chars: (v) => v.length <= 25 || 'Input too long!',
-      tmp: '',
-      search: '',
-      pagination: {},
-      /*
-      eventData: [
-        { text: 'Event', value: 'name' },
-        { text: 'Host', value: 'hostName' },
-        { text: 'Description', value: 'description' },
-        { text: 'Start Date', value: eventSplit(startTime)[0]},
-        { text: 'End Date', value: eventSplit(endTime)[0]},
-        { text: 'Start Time', value: eventSplit(startTime)[1]},
-        { text: 'End Time', value: eventSplit(endTime)[1]},
-        { text: 'Tags', value: 'tags'}
-      ], */
-      cincevents: [
-      ],
-      events: [
-          {
-            title     :  'event1',
-            start     : '2017-11-01',
-            end       : '2017-11-02',
-            cssClass  : '',
-            YOUR_DATA : {
-              thing: "stuff"
-            }
-          },
-      ],
+      demoEvents: [{
+        date: '2016/11/12', // Required
+        title: 'Foo' // Required
+      }, {
+        date: '2016/12/15',
+        title: 'Bar',
+        desc: 'description',
+        customClass: 'disabled highlight' // Custom classes to an calendar cell
+      }],
+      cincEvents: []
     }
   },
-  components : {
-  'full-calendar': require('vue-fullcalendar')
-  },
-  /*methods : {
-    'eventClick' (event, jsEvent, pos) {
-       console.log('eventClick', event, jsEvent, pos)
-    },
-    'dayClick' (day, jsEvent) {
-      console.log('dayClick', day, jsEvent)
-    },
-    'moreClick' (day, events, jsEvent) {
-      console.log('moreCLick', day, events, jsEvent)
-    }
-  },*/
-
   created() {
     axios.get('http://localhost:3000/events?approved=true').then(response => {
       var size = response.data.length;
       var i;
       for (i = 0; i < size; i++){
-        /*if (response.data[i].tags.student === 'true'){
-          this.eventData.push({
-            name: response.data[i].name,
-            host: response.data[i].hostName,
-            description: response.data[i].description,
-            startTime: response.data[i].start,
-            endTime: response.data[i].end,
-          });
-        }*/
-        if (response.data[i].tags.faculty){
-          this.cincevents.push({
+        if (response.data[i].tags.faculty || response.data[i].tags.students){
+          this.cincEvents.push({
             title: response.data[i].name,
             //host: response.data[i].hostName,
             //description: response.data[i].description,
-            start: response.data[i].start,
-            end: response.data[i].end,
+            date: response.data[i].start,
+            desc: response.data[i].description,
           });
         }
       }
     })
   },
-
 }
-
 </script>
