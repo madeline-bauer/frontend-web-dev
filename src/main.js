6c70ce4e425e9ee4f3fbf3e013157bb0b166d016
Mitch <dennison.mitch@gmail.com>
2017-12-04 10:46:09 -0500

    Delete vue-event-calendar

--- src/main.js
+++ src/main.js
@@ -9,15 +9,6 @@ import './stylus/main.styl'
 import App from './App'
 import router from './router'
 
-import 'vue-event-calendar/dist/style.css'
-import vueEventCalendar from 'vue-event-calendar'
-
-Vue.use(vueEventCalendar,
-  {
-    locale: 'en',
-    color: 'gray'
-  })
-
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
